45504bac8632cb8c5fd7ba553332f7067c006112
RMR <34992318+M0rn1ngSt4r@users.noreply.github.com>
2020-03-19 22:32:19 -0600

    like  a n00b...
    end it now...

---
+++ installnewdrupal8.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+echo "Information for new Drupal 8 site..."
+read -p "Directory Path for Drupal 7 site (remove last '/'): " sitepath
+read -p "Database username (new): " dbuser
+read -s -p "Database password(new): " dbpass
+echo
+read -p "Database ip/host(new): " dbhost
+read -p "Database name(new): " dbname
+sitedir="$(echo -n ${sitepath##*/})"
+basepath="${sitepath::-${#sitedir}}"
+echo "Creating new Drupal 8.8.4 site..."
+cd "${basepath}"
+drush dl drupal-8.8.4
+mv drupal-8.8.4 "new_${sitedir}"
+cd "new_${sitedir}"
+drush si --db-url="pgsql://${dbuser}:${dbpass}@${dbhost}/${dbname}" --account-pass="${dbpass}" --notify -y
+echo "Drupal 8 site created..."
+echo "Installing migration modules"
+drush en migrate_upgrade migrate_tools migrate_plus -y
+echo "Information of old Drupal 7 site..."
+read -p "Database username (old): " legacydbuser
+read -s -p "Database password(old): " legacydbpass
+echo
+read -p "Database ip/host(old): " legacydbhost
+read -p "Database name(old): " legacydbname
+echo "Retrieving Drupal 7 site enabled modules..."
+cd "${sitepath}"
+oldmodules="$(drush pml --type=module --status=enabled --pipe)"
+echo "Enabling modules (Drupal 8)..."
+cd "${basepath}"
+cd "new_${sitedir}"
+for module in $oldmodules; do
+    drush en "${module}" -y
+done
+echo "Preparing Migration..."
+drush migrate-upgrade --legacy-db-url="pgsql://${legacydbuser}:${legacydbpass}@${legacydbhost}/${legacydbname}" \
+    --legacy-root="${sitepath}" --configure-only
+echo "Migration status..."
+drush migrate-status
+echo "Trying Migration..."
+drush migrate-import --all
+echo "Success...?"
+echo "Transfering old .htaccess file."
+cp "${sitepath}/.htaccess" .htaccess
+echo "Information of Apache Configuration (VirtualHosts)..."
+read -p "Absolute Path for Drupal 7 site .conf file: " apachehost
+apachefile="$(echo -n ${apachehost##*/})"
+echo "Preparing .tar file transfer to new server..."
+read -p "Site url (www.example.com): " siteurl
+echo "Creating .tar file..."
+cd ..
+tar -pczf "new_${sitedir}.tar.gz" "new_${sitedir}/." --checkpoint=.100
+echo "Information for ssh comunication..."
+read -p "New Web Server ip/hostname: " webhost
+read -p "New Web server privileged user (sudoer?): " webuser
+read -s -p "New Web Server privileged user password: " webpass
+echo
+echo "Transfering .tar file..."
+sshpass -p "${webpass}" scp "new_${sitedir}.tar.gz" "${webuser}@${webhost}:~/"
+echo "Transfering .conf file (VirtualHost)..."
+sshpass -p "${webpass}" scp "${apachehost}" "${webuser}@${webhost}:~/"
+echo "Extracting/Moving Drupal 8 site to new location..."
+sshpass -p "${webpass}" ssh -o StrictHostKeyChecking=no -t "${webuser}@${webhost}" \
+    "echo ${webpass} | sudo -S sh -c 'tar -xf new_${sitedir}.tar.gz --checkpoint=.100 && \ 
+    mv new_${sitedir} /var/www/ && mv /var/www/new_${sitedir} /var/www/${sitedir}'"
+echo "Moving/Enabling .conf file to sites-available"
+sshpass -p "${webpass}" ssh -o StrictHostKeyChecking=no -t "${webuser}@${webhost}" \
+    "echo ${webpass} | sudo -S sh -c 'mv ${apachefile} /etc/apache2/sites-available && a2ensite ${apachefile} && \
+    echo "127.0.0.1 ${siteurl}" >> /etc/hosts'"
