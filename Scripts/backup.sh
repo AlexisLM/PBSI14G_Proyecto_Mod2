677f74271e1779577e7d10b7f276f1fd947d6e29
root <root@debian8>
2020-03-20 07:27:37 -0600

    Updating main script and backup.

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-# Generate backups
-echo "Generating backup for site1 ..."
-su - postgres -c "pg_dump site1 > site1db.sql"
-tar -czf site1.tar.gz -C /var/www site1 -C /var/lib/postgresql site1db.sql
+site1path="${1}"
+site2path="${2}"
+site1dir="$(echo -n ${site1path##*/})"
+site2dir="$(echo -n ${site2path##*/})"
+basepath1="${site1path::-${#site1dir}}"
+basepath2="${site2path::-${#site2dir}}"
 
-echo "Generating backup for site2 ..."
-su - postgres -c "pg_dump site2 > site2db.sql"
-tar -czf site2.tar.gz -C /var/www site2 -C /var/lib/postgresql site2db.sql
+# Generate backups
+echo "Generating backup for site1 (${site1path})..."
+#su - postgres -c "pg_dump site1 > site1db.sql"
+#tar -czf site1.tar.gz -C "${basepath1}" "${site1dir}" -C /var/lib/postgresql site1db.sql
+tar -czf site1.tar.gz -C "${basepath1}" "${site1dir}" 
+
+echo "Generating backup for site2 (${site2path})..."
+#su - postgres -c "pg_dump site2 > site2db.sql"
+#tar -czf site2.tar.gz -C /var/www site2 -C /var/lib/postgresql site2db.sql
+tar -czf site2.tar.gz -C "${basepath2}" "${site2dir}" 
 
 # Delete db backups
-rm /var/lib/postgresql/site1db.sql /var/lib/postgresql/site2db.sql
+#rm /var/lib/postgresql/site1db.sql /var/lib/postgresql/site2db.sql
 
 echo "Backups successfully generated!"
 
--- Scripts/drupal_migration_tool.sh
+++ Scripts/drupal_migration_tool.sh
@@ -1,41 +1,49 @@
 #!/bin/bash
 
-echo -e "\nGenerating backups..."
-./backup.sh
+# Check sites
+echo -e "\nChecking if the sites exist..."
+read -p "Directory Path for Drupal 7 site1 (remove last '/'): " site1path
 
+if [[ -d "${site1path}" ]]; then
+  echo "Site1: OK"
+else
+  echo -e "Site1 (${site1path}) doesn't exist.\nExiting..."
+  exit 1
+fi
+
+read -p "Directory Path for Drupal 7 site2 (remove last '/'): " site2path
+if [[ -d "${site2path}" ]]; then
+  echo "Site2: OK"
+else
+  echo -e "Site2 $(site2path) doesn't exist.\nExiting..."
+  exit 1
+fi
+
+# Generate backups
+echo -e "\nGenerating backups..."
+./backup.sh "${site1path}" "${site2path}"
+
+# Check dependencies
 echo -e "\nChecking dependencies..."
 ./verify_install_git-drush.sh
 
-echo -e "\nChecking if the sites exist..."
-if [[ -d "/var/www/site1" ]]; then
-  echo "Site1: OK"
-else
-  echo -e "Site1 doesn't exist.\nExiting..."
-  exit 1
-fi
-
-if [[ -d "/var/www/site2" ]]; then
-  echo "Site2: OK"
-else
-  echo -e "Site2 doesn't exist.\nExiting..."
-  exit 1
-fi
-
+# Disable modules
 #echo -e "\nDisabling modules..."
 #./disable_modules.sh
 
-# Renaming sites
-echo -e "\nRenaming site1 to site1_old"
-mv /var/www/site1 /var/www/site1_old
+# Rename sites
+echo -e "\nRenaming sites..."
+echo -e "Renaming ${site1path} to ${site1path}_old..."
+mv ${site1path} ${site1path}_old
 
-echo -e "\nRenaming site2 to site2_old"
-mv /var/www/site2 /var/www/site2_old
+echo -e "Renaming ${site2path} to ${site2path}_old..."
+mv ${site2path} ${site2path}_old
 
 # Install new sites
-echo -e "\nInstalling new sites"
-echo -e "\nInstalling site1"
-./install_site.sh "site1" "adminsite1" "databasesite1"
+echo -e "\nInstalling new sites..."
+echo -e "\nInstalling site1..."
+#./install_site.sh "site1" "adminsite1" "databasesite1"
 
-echo -e "\nInstalling site2"
-./install_site.sh "site2" "adminsite2" "databasesite2"
+echo -e "\nInstalling site2..."
+#./install_site.sh "site2" "adminsite2" "databasesite2"
 
