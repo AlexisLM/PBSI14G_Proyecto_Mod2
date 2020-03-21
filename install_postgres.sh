d2b58fbc74887d022315b07bb494d4e8971ab1bd
Alexis BLM <alexis-blm@ciencias.unam.mx>
2020-03-20 18:20:07 -0600

    Before merge

--- install_postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt -y install postgresql-11
-sudo pg_ctlcluster 11 main start
-sudo mv pg_hba.conf /etc/postgresql/11/main/
-sudo chown postgres:postgres /etc/postgresql/11/main/pg_hba.conf
-sudo systemctl restart postgresql
